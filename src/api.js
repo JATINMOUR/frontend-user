67fa9ba6eaabf909372fa3f6da863fd19833ceb2
JΛƬIП Mourya <102876688+JATINMOUR@users.noreply.github.com>
2026-01-07 12:20:20 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-beta-silk-33.vercel.app/api"
+  baseURL: "https://backend-beta-silk-33.vercel.app/api/reviews"
 });
 
 export default api;
