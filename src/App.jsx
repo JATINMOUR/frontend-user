191170cdca85684e3359564d7fff2a2995105a5e
JΛƬIП Mourya <102876688+JATINMOUR@users.noreply.github.com>
2026-01-07 11:55:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+// import ReviewForm from "./components/ReviewForm";
 import Dashboard from "./components/Dashboard";
 import UserFeedback from "./components/UserFeedback";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* USER DASHBOARD */}
-        <Route path="/" element={<UserFeedback />} />
+    <Routes>
+      {/* USER DASHBOARD */}
+      <Route path="/" element={<UserFeedback />} />
 
-        {/* ADMIN DASHBOARD */}
-        <Route path="/admin" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+      {/* ADMIN DASHBOARD */}
+      <Route path="/admin" element={<Dashboard />} />
+    </Routes>
   );
 }
 
